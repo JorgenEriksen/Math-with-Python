a785d7ecd9c836fc6a5e318ec6ae6c436c0a7c55
Jorgen Eriksen <jorgen.eriksen93@gmail.com>
2020-03-01 02:52:19 +0100

    test

---
+++ test/temp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+name = "John Smit"
+year = 20;
+newPatien = True;
+
+print("name: " + name);
+print("year: " + year)
+
+
+name = input("What is your name? ")
+
+print(price * 10)
